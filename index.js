fb1296ca526520e46fff650fa84afca579c13ec5
miguelAngelVM <miguelangelvm@miguel.web>
2018-03-10 13:42:09 -0600

    Se agregaron los estilos y funciones llamadas por eventos en react

--- index.js
+++ index.js
@@ -4,4 +4,9 @@ import Media from './src/playlist/componente/media'
 
 const app =  document.getElementById('app')
 
-render(<Media/>, app)
+render(<div><Media title="Diseño responsivo" 
+            images="./images/covers/responsive.jpg"
+            author="Miguel Angel Ventura Marin" />
+            <Media title="Diseño responsivo" 
+            images="./images/covers/responsive.jpg"
+            author="Miguel Angel Ventura Marin" /></div>, app)
--- src/playlist/componente/media.js
+++ src/playlist/componente/media.js
@@ -1,20 +1,45 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import './media.css';
 
 class Media extends Component{
+    constructor(props){
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick = (event) => {
+        console.log(this.props.title);
+    }
     render(){
+        const styles = {
+            container: {
+              color: '#44546b',
+              cursor: 'pointer',
+              width: 260,
+              border: '1px solid red'
+            }
+        }
         return(
-            <div>
-                <div>
+            <div className='Media' onClick={this.handleClick}>
+                <div className='Media-cover'>
                     <img 
-                        src="" 
+                        src={this.props.images} 
                         atl="" 
                         width={260}
                         height={160}/>
                 </div>
-                <h3>Titulo del video </h3>
-                <p>Autor</p>
+                <h3 className="Media-title">{this.props.title}</h3>
+                <p className="Media-author">{this.props.author}</p>
             </div>
         )
     }
 } 
+
+Media.propTypes = {
+    cover: PropTypes.string,
+    title: PropTypes.string.isRequired,
+    author: PropTypes.string,
+    type: PropTypes.oneOf(['video', 'audio']),
+}
+  
 export default Media
