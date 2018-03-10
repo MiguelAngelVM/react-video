9282c6f8241732027bacfc9a158b137989b7d26d
miguelAngelVM <miguelangelvm@miguel.web>
2018-03-10 12:14:16 -0600

    inicio de la aplicacion de videos en react

---
+++ src/playlist/componente/media.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+class Media extends Component{
+    render(){
+        return(
+            <div>
+                <div>
+                    <img 
+                        src="" 
+                        atl="" 
+                        width={260}
+                        height={160}/>
+                </div>
+                <h3>Titulo del video </h3>
+                <p>Autor</p>
+            </div>
+        )
+    }
+} 
+export default Media
