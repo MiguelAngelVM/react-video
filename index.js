b89565bfdfeb0652567441d42c1a0ac422dd235e
miguelAngelVM <miguelangelvm@miguel.web>
2018-03-11 22:09:58 -0600

    listas con map

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 import React from 'react'
 import {render} from 'react-dom'
-import Media from './src/playlist/componente/media'
+import PlayList from './src/playlist/componente/playlist'
+import data from './src/api.json'
 
 const app =  document.getElementById('app')
 
-render(<div><Media title="Diseño responsivo" 
-            images="./images/covers/responsive.jpg"
-            author="Miguel Angel Ventura Marin" />
-            <Media title="Diseño responsivo" 
-            images="./images/covers/responsive.jpg"
-            author="Miguel Angel Ventura Marin" /></div>, app)
+render(<PlayList data={data}/>, app)
--- src/playlist/componente/media.js
+++ src/playlist/componente/media.js
@@ -1,8 +1,8 @@
-import React, {Component} from 'react';
+import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
 import './media.css';
 
-class Media extends Component{
+class Media extends PureComponent{
     constructor(props){
         super(props);
         this.handleClick = this.handleClick.bind(this);
@@ -23,7 +23,7 @@ class Media extends Component{
             <div className='Media' onClick={this.handleClick}>
                 <div className='Media-cover'>
                     <img 
-                        src={this.props.images} 
+                        src={this.props.cover} 
                         atl="" 
                         width={260}
                         height={160}/>
@@ -43,3 +43,49 @@ Media.propTypes = {
 }
   
 export default Media
+
+
+
+/*
+	constructor(){
+	// Enlazo (bind) eventos y/o inicializo estado
+	}
+	componentWillMount(){
+	// Se ejecuta antes de montar el componente
+	// Se podría usar para hacer un setState()
+	}
+	render(){
+	// Contiene todos los elementos a renderizar
+	// podrías usarlo para calcular propiedades (ej: concatenar una cadena)
+	}
+	componentDidMount(){
+	//Solo se lanza una vez
+	//Ideal para llamar a una API, hacer un setInteval, etc
+	}
+
+	//Actualización:
+	componentWillReceiveProps(){
+	//Es llamado cuando el componente recibe nuevas propiedades.
+
+	}
+	shouldComponentUpdate(){
+	//Idea para poner una condición y  si las propiedades que le llegaron anteriormente
+	// eran las mismas que tenia retornar false para evitar re-renderear el componente
+	}
+	componentWillUpdate(){
+	//metodo llamado antes de re-renderizar el componente si shouldComponentUpdate devolvió true
+	}
+
+	// re-render si es necesario...
+
+	componentDidUpdate(){
+	//Método llamado luego del re-render
+	}
+	componentWillUnmount(){
+	//Método llamado antes de desmontar el componente
+	}
+	componentDidCatch(){
+	// Si ocurre algún error, lo capturo desde acá:
+	}
+
+*/
